9afe5ef3532ed2f9a46734ec3ab9c5874b8e7ce3
andresgarbarz <elianatangarifeblandon@gmail.com>
2025-12-19 11:11:15 -0500

    fix: ux butttom CTA

--- src/components/Navbar&Footer/Navbar.tsx
+++ src/components/Navbar&Footer/Navbar.tsx
@@ -25,7 +25,7 @@ function Navbar() {
         <Link href="/projects" className={linkClasses("/projects")}>Proyectos</Link>
 
         <a href="https://mail.google.com/mail/?view=cm&to=elianatangarifeblandon@gmail.com" target="_blank" rel="noopener noreferrer"
-          className="px-2 md:px-4 py-2 bg-white/90 rounded-lg text-neutral-800 font-semibold shadow-sm hover:bg-white hover:shadow-md transition duration-300"
+          className="px-3 md:px-5 py-2  bg-purple-300 hover:bg-cyan-300 rounded-lg text-neutral-800 font-semibold shadow-md hover:shadow-lg hover:-translate-y-0.5  transition-all duration-300"
         >
           Contáctame
         </a>
